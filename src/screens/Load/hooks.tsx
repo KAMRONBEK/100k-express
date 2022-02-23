bcadaee79b03bf5eb0fecd124f57868fc136d4d8
muhamadmansurov <muhammadibnmaruf1@gmail.com>
2022-02-23 20:56:04 +0500

    uploading changes

--- src/screens/Load/hooks.tsx
+++ src/screens/Load/hooks.tsx
@@ -6,106 +6,153 @@ import reactotron from "reactotron-react-native";
 import { requests } from "../../api/requests";
 import { routes } from "../../navigation/routes";
 import {
-    selectBuyContact,
-    selectCommonLoad,
-    selectLoad,
-    setBuyContact,
-    setCommonLoad,
-    setLoad,
+  selectCommonLoad,
+  selectLoad,
+  selectSeenLoad, setCommonLoad, setLoad, setSeenLoad
 } from "../../redux/slices/load/load";
 import { selectUser } from "../../redux/slices/user/user";
 
+enum loadOrderType {
+  new = "new",
+  active = "active",
+  newest = "asd"
+}
+
 export let useLoadHook = () => {
-    let load = useSelector(selectLoad);
-    let commonLoad = useSelector(selectCommonLoad);
-    let buyContact = useSelector(selectBuyContact)
-    let navigation = useNavigation();
-    const [loading, setLoading] = useState(false);
-    let user = useSelector(selectUser);
-    let dispatch = useDispatch();
-    let effect = async () => {
-        try {
-            let res = await requests.load.getLoad();
-            dispatch(setLoad(res.data.data));
+  let load = useSelector(selectLoad);
+  let commonLoad = useSelector(selectCommonLoad);
+  let seenLoad = useSelector(selectSeenLoad)
+  let navigation = useNavigation();
+  const [loading, setLoading] = useState(false);
+  let user = useSelector(selectUser);
+  let dispatch = useDispatch();
 
-            let resCommon = await requests.load.getCommonLoad();
-            dispatch(setCommonLoad(resCommon.data.data));
+  const loadLoad = async () => {
+    setLoading(true)
+    try {
+      let res = await requests.load.user.getLoad(loadOrderType.newest);
+      dispatch(setLoad(res.data.data));
+    }
+    catch (err: any) {
+      showMessage({ message: JSON.stringify(err.response.data) + "\n error in load", type: 'danger' })
+    }
+    finally {
+      setLoading(false)
+    }
+  }
 
-            let resBuyContact = await requests.load.getLoadBuyContact();
-            dispatch(setBuyContact(resBuyContact.data.data))
-        } catch (err) {
-            console.log(err.response);
-            showMessage({
-                message: JSON.stringify(err.response),
-                type: "danger",
-                icon: "danger",
-                floating: true,
-            })
-        }
-    };
-    useEffect(() => {
-        effect();
-    });
+  const loadCommonLoad = async () => {
+    setLoading(true)
+    try {
+      let res = await requests.load.common.getCommonLoad();
+      dispatch(setCommonLoad(res.data.data));
+    }
+    catch (err: any) {
+      showMessage({ message: JSON.stringify(err.response.data) + "\n error in common load", type: 'danger' })
+    }
+    finally {
+      setLoading(false)
+    }
+  }
 
-    const refreshLoad = () => {
-        effect();
-    };
+  const loadSeenLoad = async () => {
+    setLoading(true)
+    try {
+      let res = await requests.load.driver.getLoad(loadOrderType.active);
+      dispatch(setSeenLoad(res.data.data));
+    }
+    catch (err: any) {
+      showMessage({ message: JSON.stringify(err.response.data) + "\n error in seen load", type: 'danger' })
+    }
+    finally {
+      setLoading(false)
+    }
+  }
 
-    const createLoad = async (credentials) => {
-        setLoading(true);
-        try {
-            let res = await requests.load.createLoad(credentials);
-            showMessage({
-                message: "Zakaz qabul qilindi",
-                type: "success",
-                icon: "success",
-                floating: true,
-            });
-            navigation.goBack();
-        } catch (error) {
-            reactotron.log!(error);
-            showMessage({
-                message: error.message,
-                type: "danger",
-                icon: "danger",
-                floating: true,
-            });
-        } finally {
-            setLoading(false);
-        }
-    };
+  let effect = () => {
+    loadLoad()
+    loadCommonLoad()
+    loadSeenLoad()
+  };
 
-    const editLoad = async (credentials, id) => {
-        setLoading(true);
-        try {
-            let res = await requests.load.createLoad(credentials, id);
-            showMessage({
-                message: "Zakaz qabul qilindi",
-                type: "success",
-                icon: "success",
-                floating: true,
-            });
-            navigation.navigate(routes.LOAD);
-        } catch (error) {
-            reactotron.log!(error);
-            showMessage({
-                message: error.message,
-                type: "danger",
-                icon: "danger",
-                floating: true,
-            });
-        } finally {
-            setLoading(false);
-        }
-    };
+  let useLoadRefresh = () => {
+    loadLoad()
+  };
+  let useCommonLoadRefresh = () => {
+    loadCommonLoad()
+  };
+  let useSeenLoadRefresh = () => {
+    loadSeenLoad()
+  };
 
-    return {
-        load,
-        commonLoad,
-        refreshLoad,
-        createLoad,
-        loading,
-        editLoad,
-        buyContact,
-    };
+
+  const onConnect = async (id) => {
+    let res = await requests.load.getSeenLoad(id);
+  }
+
+  useEffect(() => {
+
+    effect();
+  }, []);
+
+  const createLoad = async (credentials) => {
+    setLoading(true);
+    try {
+      let res = await requests.load.user.createLoad(credentials);
+      showMessage({
+        message: "Zakaz qabul qilindi",
+        type: "success",
+        icon: "success",
+        floating: true,
+      });
+      navigation.goBack();
+    } catch (error) {
+      reactotron.log!(error);
+      showMessage({
+        message: error.message,
+        type: "danger",
+        icon: "danger",
+        floating: true,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const editLoad = async (credentials, id) => {
+    setLoading(true);
+    try {
+      let res = await requests.load.user.editLoad(credentials, id);
+      showMessage({
+        message: "Zakaz qabul qilindi",
+        type: "success",
+        icon: "success",
+        floating: true,
+      });
+      navigation.navigate(routes.LOAD);
+    } catch (error) {
+      reactotron.log!(error);
+      showMessage({
+        message: error.message,
+        type: "danger",
+        icon: "danger",
+        floating: true,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    load,
+    commonLoad,
+    createLoad,
+    loading,
+    editLoad,
+    seenLoad,
+    useLoadRefresh,
+    useCommonLoadRefresh,
+    useSeenLoadRefresh,
+    onConnect
+  };
 };
